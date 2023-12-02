693a4887b06dcee79bedc2890074ae771b02cb73
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-12-02 20:02:35 +0200

    push

--- index.js
+++ index.js
@@ -1,117 +1,102 @@
-const int = 11;
+// const table = {
+//   nazva: "Table",
+//   material: "derevo",
+// };
 
-// String ing = "asd".length > "asdf".length;
-if (int % 2 === 0) {
-  console.log(int % 2 === 0);
-  console.log("парне");
-} else {
-  console.log(int % 2 === 0);
-  console.log(" не парне");
-}
+// const head = {
+//   meta: "",
+// };
+// document.querySelector = дозволяє нам знайти та записати у змінну будь-який html тег на сторінці. Якщо тегів за пошуковим запитом багато то обереться перший який співпав
+const ulAttribute = document.querySelector("[ul]");
+const ulClass = document.querySelector(".ul");
+const ulId = document.querySelector("#ul");
+const ulTag = document.querySelector("ul");
+// .childnodes дозволяє нам отримати масив дитячих елементів але з нюансами(включаючи текст тобіш пробіли,переноси строк)
+const child = ul.childNodes;
+//.children = повертає масив дитячих тегів.
 
-// ------------------------- Логічні опертаори ------------
+const children = ul.children;
 
-// ЛОГІЧНИЙ && (і)
-// чи можемо ми дати знижку на товари від 10000 до 20000
-const bread = 10000;
-const tovar = 15000;
-const tv = 30000;
-const pc = 120000;
-const noCan = tv >= 10000 && tv <= 20000;
-const can = tovar >= 10000 && tovar <= 20000;
-const noNoCan = pc >= 10000 && pc <= 20000;
+// .firstchild = повертає перший дитячий елемент якщо ви дотримуєтись хоча б яких потреб кодстайлінгу то скоріш за все це перенос строки.
+const first = ul.firstChild;
+// .firstElementChild = повертає перший дитячий елемент.
 
-// логічний оператор && повертає false якщо хочаб один вираз не правдивий(false) якщо перша операція = false то друга або третя навіть не прочитається
-const canBread = bread >= 10000 && bread <= 20000 && bread === 10001;
-console.log(noNoCan);
-console.log(can);
-console.log(canBread);
+const realyFisrt = ul.firstElementChild;
 
-// ЛОГІЧНИЙ АБО (||)
+// .firstchild = повертає останній дитячий елемент якщо ви дотримуєтись хоча б яких потреб кодстайлінгу то скоріш за все це перенос строки.
+const last = ul.lastChild;
+// .lastElementChild = повертає останній дитячий елемент.
 
-// Або нам вистачає грошей на гарну пеку або на трошки гірше і погуляти
+const realyLast = ul.lastElementChild;
 
-const peca = 50000;
-const pekaNotBad = 40000;
+// .nextElementSibling; = дозволяє взяти  сусіда попереду
 
-const walk = peca > 50001 || (pekaNotBad > 30000 && tovar <= 10000);
-console.log(walk);
+const midleLi = realyFisrt.nextElementSibling;
 
-const num = 7;
+// .previousElementSibling; = дозволяє взяти попереднього сусіда
 
-// оператор АБО (||) якщо хоча б одне значення є true то результат логічного порівнняння буде true
-const result = num < 10 || num > 30 || num < 3;
-console.log(result);
-// ЛОГІЧНЕ НЕ (!)
-// Оператор НЕ(!) дозволяє отримати протилежне значення або щось заперечити
+const midleLiFromLast = realyLast.previousElementSibling;
 
-console.log(!true);
+console.log(midleLiFromLast);
 
-console.log(!0);
+// ПОШУК ЕЛЕМЕНТІВ
 
-console.log(!Boolean(56));
+const li = document.querySelector(".li");
 
-// Щоб відпочити потрібні квитки і полетіти відпочивати якщо квитків немає то відпочити не вийде
-console.log(false && bread === 10000);
+const previous = li.parentElement;
 
-console.log(false || true || false);
+const nextSibling = li.nextElementSibling;
 
-const sub = 500;
-const standart = 100;
-const silver = 150;
-const gold = 200;
-const platinum = 250;
-const iron = 500;
+// document.querySelectorAll дозволяє знайти усі елементи на сторінці за заданими параметрами(Знаходить у вигляді масиву)
+const manyLi = document.querySelectorAll(".li");
+manyLi[0].attributes.src = "s;ldf,f;lsd,f";
 
-console.log(sub >= platinum);
+manyLi[1].attributes.src =
+  "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1701475200&semt=sph";
+console.log(manyLi);
 
-// якщо умова всередені if не виконується(false) то і увесь блок коду який ви написали в if  не виконується
-if (sub >= platinum) {
-  const num = 2 + 2 + 3;
-  console.log(num);
+// document .querySelector дозволяє нам знайти html тег на нашій сторінці
+const img = document.querySelector(".li-img");
+// Звернення до атрибутів картинки src та alt для їх переприсвоєння значення
 
-  // Else = виконується якщо блок IF не виконався
-} else if (sub >= gold) {
-  console.log(
-    "Шановний саб, у вас є можливість купити голдову підписку зі знижкою"
-  );
-} else if (sub >= silver) {
-  console.log(" Ну хоча сільвер купи");
-} else {
-  console.log(
-    "В тебе має бути хоча б стандартна підписка бо нам треба грошики"
-  );
-}
+img.alt = "Картинка";
+img.src =
+  "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1701475200&semt=sph";
+console.log(img);
 
-switch (sub) {
-  case 100:
-    console.log(standart);
-    break;
-  case 200:
-    console.log(silver);
-    break;
-  case 300:
-    console.log(gold);
-    break;
-  case 400:
-    console.log(platinum);
-    break;
-  case 500:
-    console.log(iron);
-    break;
+const input = document.querySelector("#input").value;
+console.log(input);
 
-  default:
-    console.log("ЗАЖРАЛИСЬ");
-    break;
-}
+const checkbox = document.querySelector(".checkbox");
 
-// ТЕРНАРНИЙ ОПЕРАТОР
-const wage = 101;
-if (wage <= 100) {
-  console.log("ok");
-} else {
-  console.log("Їди додому");
-}
-// Тернарний оператор створений для простих задач,для виконання невиличкої умови, її перевірки за допомогою знаку питання і повернення двух значень якщо правда повертається перше значення після знаку питання якщо не правда повертається значення після двокрапки.
-const isOk = wage <= 100 ? "ok" : "Їди додому";
-console.log(isOk);
+console.log(checkbox.checked);
+
+//ТЕКСТОВИЙ КОНТЕНТ
+
+const p = document.querySelector(".p");
+console.log(p.textContent);
+// .textContent дозволяє отримати текстовий контент та при бажанні його замінити
+p.textContent = "Привіт";
+console.log(p.textContent);
+
+// СТИЛІ(Інлайнові)
+
+document.body.style.backgroundColor = "DarkBlue";
+
+const button = document.querySelector(".button");
+
+button.style.backgroundColor = "yellow";
+
+// ClassList додавання класівʼ
+const pp = document.querySelector(".pp");
+// .add додати клас
+pp.classList.add("bbb");
+
+console.log(pp);
+// remove видалити клас
+// pp.classList.remove("bbb");
+console.log(pp);
+// .contains повертає true або false в залежності від того чи є клас на який ви запитали на елементі якщо є true якщо нема false
+console.log(pp.classList.contains("bbb"));
+// replace  приймає два аргумента. перший для видалення другий новий який треба підставити замість старого
+pp.classList.replace("bbb", "rrr");
