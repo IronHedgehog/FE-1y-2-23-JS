423e2dc453c893d579d71b02e7c501fd09a75b94
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2023-12-30 19:46:23 +0200

    7

--- js/array.js
+++ js/array.js
@@ -1,103 +1,77 @@
-// 1) Що таке масив?
-// 2) Як з ним працювати?
-// 3) Навчимось перебирати масив
+const string = "Hello world java script once to be";
 
-const array = [];
+// Метод split дозволяє розділити строку на масив вказавши розділювач за яким елементи мають додаватись в масив
+console.log(string.split(" "));
 
-const itemsOnCart = ["orange", "banana", "apple"];
-// Масив = індексована колекція
-// індекси розразовуються з 0 тому перший елемент в масиві має індекс 0
-// orange має індекс - 0
-// banana має індекс - 1
-// apple має індекс - 2
+// Метод join збирає з масиву строку,в дужках маємо вказати через який символ додавати елементи
+const hello = ["Hello", "World"];
+console.log(hello.join(" "));
 
-itemsOnCart[3] = "pear";
+// Метод indexOf  повертає індекс елементу який ви шукаєте у круглих дужках або -1 якщо елементу який ви шукаєте не існує.
+//Чутливий до регістру
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-itemsOnCart[100] = "boasds";
+console.log(numbers.indexOf(1));
 
-console.log(itemsOnCart[0]);
-console.log(itemsOnCart[1]);
-console.log(itemsOnCart[2]);
-console.log(itemsOnCart[3]);
-console.log(itemsOnCart[100]);
-// властивість length повертає довжину масиву(кількість елементів масиву)
-itemsOnCart.length = 50;
-console.log(itemsOnCart);
+const names = ["Polly", "Ajax", "Mango"];
+let winner;
 
-const numbers = [
-  1, 2, 3, 4, 5, 5, 6, 7, 6, 4, 4, 3, 3, 2, 1, 1, 23, 4, 5, 5, 3, 3, 22, 1, 23,
-  3, 123, 123, 123, 12, 312, 5, 34, 5, 2341, 23,
-];
-
-const arr = [];
-// лічильник(i)  стартує з 0, довжина масиву стартує з 1
-
-for (let i = 1; i <= 10; i += 1) {
-  // .push() додає елементи у кінець масиву
-  arr.push(i);
+// метод includes перевіряє чи є такий елемент в масиві  якщо є повертає true якщо нема  false
+//Чутливий до регістру
+console.log(names.includes("Polly"));
+for (const name of names) {
+  if (name.toLowerCase().includes("polly")) {
+    winner = name;
+    break; // зупиняє цикл
+  }
+  console.log(name);
 }
-// console.log(arr);
 
-let sum = 0;
+console.log(winner);
 
-// for (let number of numbers) {
-//   //   sum = sum + number;
-//   if (number % 3 === 0) {
-//     sum += number;
-//   }
-//   console.log("Число не парне");
-//   break;
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+for (const number of arr2) {
+  // метод push дозволяє нам додавати елементи в кінець масиву
+  arr1.push(number);
+  // метод pop видаляє елемент з кінця масиву
+  arr2.pop(6);
+}
+// for (const number of arr1) {
+//   // shift метод який видаляє елементи з початку масиву
+//   //unshift метод який додає елементи в початок масиву
+//   arr2.shift(1);
 // }
-// console.log(sum);
 
-const clients = [
-  "Artem",
-  "Mykola",
-  "Ivan",
-  "Ivan",
-  "Ivan",
-  "Ivan",
-  "Ivan",
-  "Ivan",
-];
+console.log(arr2);
 
-const nameToFind = "Ivan";
-for (const client of clients) {
-  if (client === nameToFind) {
-    console.log("ми знайшли тебе");
-    break;
-  }
-  console.log(" це новий користувач");
-}
+// Нам приходить з бази данних масив користувачив
 
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-// Переприсвоєння першого елементу підмасиву
-// console.log((matrix[0][0] = 10));
+const users = ["Artem", "Perto", "Anton"];
 
-let result = 0;
+const newUser = "Ajax";
 
-for (let i = 0; i < matrix.length; i++) {
-  console.log(matrix[i]);
-  for (let j = 0; j < matrix[i].length; j++) {
-    console.log(matrix[i][j]);
-    result += matrix[i][j];
-  }
-}
-console.log(result);
+// Метод slice повертає нам новий масив
+// Дозволяє зробити копію масиву щоб не пошкодити початковий масив данних
+// Метод slice приймає два параметри Start(індекс початку) та END(індекс кінця(НЕ ВКЛЮЧНО))
 
-const arrrrr = [null, undefined, "10", null, 10, 15, 20, {}, [1, 2, 3]];
-const resultt = 0;
+// .slice() = slice(0, users.length)
+const users1 = users.slice(0, users.length);
 
-for (const number of arrrrr) {
-  if (number === null || number === undefined) {
-    continue;
-  }
-  result += Number.parseInt(number);
-  console.log(result);
-}
+users1.push(newUser, newUser);
+console.log(users1);
+// Метод splice який повертає всі видалені елементи
+// Перший параметр це значення звідки ми почінаємо вирізати елементи, Другий параметр це параметр який означає скільки елементів треба вирізати після вказаного в першому параметрі індексі
+// Третій параметр дозволяє щось додати замість видалених елементів(ПАраметрів для додавання може бути безліч треба перелічуват`и через ком`у )
 
-console.log(result);
+const deletedUser = users.indexOf("Artem");
+users.splice(deletedUser, 1, "Anton", "Чудова людина");
+console.log(users);
+
+const arr3 = [1, 2, 3];
+const arr4 = [4, 5, 6];
+
+// Метод concat слугує для обʼєднання двох масивів (Масив який ви додаєте буде додаватись в кінець іншого)
+const arr5 = arr3.concat(arr4);
+console.log(arr5);
